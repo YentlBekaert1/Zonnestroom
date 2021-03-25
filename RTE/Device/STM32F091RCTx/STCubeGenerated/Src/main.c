0be289a008d66a59343504b581ba3aa2f1718297
Yentl Bekaert <yentl.bekaert@student.vives.be>
2021-03-25 09:38:29 +0100

    initial

--- RTE/Device/STM32F091RCTx/STCubeGenerated/Src/main.c
+++ RTE/Device/STM32F091RCTx/STCubeGenerated/Src/main.c
@@ -189,7 +189,7 @@ unsigned char Vives6[] = {
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
